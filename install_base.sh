0527f03b1c7435307f673d6b183fb7ae9810f4a4
user <arlrcode@gmail.com>
2021-04-22 10:12:11 +0200

    update the install

--- install_base.sh
+++ install_base.sh
@@ -65,14 +65,27 @@ sudo apt install -y python3-pip
 
 
 # From git
-
+BLUE "Download bin2img..."
+git clone https://github.com/c3er/bin2img.git /home/`logname`/Software/bin2img/
+GREEN "Install bin2img..."
+pip3 install -r /home/`logname`/Software/bin2img/requirements.txt
 
 # From web
 BLUE "Download hash-identifier..."
 wget https://storage.googleapis.com/google-code-archive-source/v2/code.google.com/hash-identifier/source-archive.zip -O /home/`logname`/Software/hash-identifier-archive.zip
 7z x /home/`logname`/Software/hash-identifier-archive.zip -o/home/`logname`/Software/
 
+BLUE "Download signsrch..."
+wget http://aluigi.org/mytoolz/signsrch.zip -O /home/`logname`/Software/signsrch.zip
+7z x /home/`logname`/Software/signsrch.zip -o/home/`logname`/Software/signsrch
+GREEN "Install signsrch..."
+make -C /home/`logname`/Software/signsrch/src
+cp /home/`logname`/Software/signsrch/src/signsrch /home/`logname`/Software/signsrch/
 
 # End
 chmod -R o+wxr /home/`logname`/Software/
 
+
+
+
+GREEN "Install over"
